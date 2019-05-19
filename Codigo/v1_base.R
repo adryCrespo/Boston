c29f22986940c13b40627ab638b5c5ac5a881574
adry <adrian.crespo.durante@gmail.com>
2019-05-19 14:22:22 +0200

    Datos

--- Codigo/v1_base.R
+++ Codigo/v1_base.R
@@ -3,7 +3,8 @@ library(data.table)#Importacion datos
 library(DataExplorer)#Exploracion datos automatico
 
 #Importar datos ----
-data1    <- as.data.frame(fread('Hdma.csv')) #datos training
+data1    <- as.data.frame(fread('Datos\\Hdma.csv')) #datos training
 create_report(data1, y = "deny")# crea informe en fotos
-sum(is.na(data1$deny))
+
+
 apply(is.na(data1),2,sum)
